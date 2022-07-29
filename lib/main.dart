a7dfb02e5404d8404e909fb14708d393f68dcc71
abdelrahman-abdelghany1 <104377203+abdelrahman-abdelghany1@users.noreply.github.com>
2022-07-29 15:37:56 +0200

    responsive_framework

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:petology/screens/kind_screen.dart';
 import 'package:petology/screens/login_screen.dart';
 import 'package:petology/themes/colors.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,6 +35,20 @@ class MyApp extends StatelessWidget {
             statusBarColor: Colors.white,
             statusBarIconBrightness: Brightness.dark,
           ),
+        ),
+      ),
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(390, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+        background: Container(
+          color: Color(0xFFF5F5F5),
         ),
       ),
       home: LoginScreen(),
